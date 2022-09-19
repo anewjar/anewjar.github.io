7e020caf9a17f7ed706de86a17ba297adbe09114
anewjar <76667402+anewjar@users.noreply.github.com>
2022-09-19 21:47:35 +0100

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -3,7 +3,7 @@ if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
     $email_to = "anuja.rajapakssa@gmail.com";
-    $email_subject = "Client query";
+    $email_subject = "New form submissions";
 
     function problem($error)
     {
